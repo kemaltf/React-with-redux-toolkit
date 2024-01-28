b27269effbd2dbb9bee58b95a26661d131c09fb5
i am just learner <fikri2701@gmail.com>
2024-01-28 18:11:29 +0000

    getting know with useDispatch

--- react-reduct-toolkit/src/features/cake/cakeView.jsx
+++ react-reduct-toolkit/src/features/cake/cakeView.jsx
@@ -1,15 +1,24 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { ordered, restocked } from "../cake/cakeSlice";
+
 export const CakeView = () => {
+  const [value, setValue] = React.useState(1);
   // state refer to the store (inside the store.js we can see several
   // reducer (aka: cake,inceream, user)) and numOfCakes is taken from
   // cakeSlice inside initialState.
   const numOfCakes = useSelector((state) => state.cake.numOfCakes);
+  const dispatch = useDispatch();
   return (
     <div>
       <h2>number of cakes - {numOfCakes}</h2>
-      <button>Order cake</button>
-      <button>Restock cake</button>
+      <button onClick={() => dispatch(ordered())}>Order cake</button>
+      <input
+        type="number"
+        value={value}
+        onChange={(e) => setValue(parseInt(e.target.value))}
+      />
+      <button onClick={() => dispatch(restocked(value))}>Restock cake</button>
     </div>
   );
 };
--- react-reduct-toolkit/src/features/icecream/icecreamView.jsx
+++ react-reduct-toolkit/src/features/icecream/icecreamView.jsx
@@ -1,14 +1,23 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { ordered, restocked } from "../icecream/icecreamSlice";
 
 export const IcecreamView = () => {
+  const [value, setValue] = React.useState(1);
   const numOfIceCream = useSelector((state) => state.icecream.numOfIcecreams);
-
+  const dispatch = useDispatch();
   return (
     <div>
       <h2>number of icecreams - {numOfIceCream}</h2>
-      <button>Order icecream</button>
-      <button>Restock icecream</button>
+      <button onClick={() => dispatch(ordered())}>Order icecream</button>
+      <input
+        type="number"
+        value={value}
+        onChange={(e) => setValue(parseInt(e.target.value))}
+      />
+      <button onClick={() => dispatch(restocked(value))}>
+        Restock icecream
+      </button>
     </div>
   );
 };
