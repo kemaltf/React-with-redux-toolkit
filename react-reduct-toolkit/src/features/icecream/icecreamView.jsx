b19fb7526840f7fdf81986410b45ca19fcc0cc80
i am just learner <fikri2701@gmail.com>
2024-01-28 17:33:07 +0000

    create view for each page

---
+++ react-reduct-toolkit/src/features/icecream/icecreamView.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const IcecreamView = () => {
+  return (
+    <div>
+      <h2>number of icecreams - </h2>
+      <button>Order icecream</button>
+      <button>Restock icecream</button>
+    </div>
+  );
+};
